47ac854be1e3eb05bba443b287a546a2a35d9ff5
marya1313 <63359536+marya1313@users.noreply.github.com>
2020-04-15 08:17:57 -0500

    tarea. Modificacion

--- main.js
+++ main.js
@@ -3,18 +3,25 @@
  console.log(elemento);
  const palabras =[];
 console.log(document.querySelector('#guarda'));
+let codeHTML="";
 
 function imprimetxt(){
+    
     alert(elemento.value);
+    valor = elemento.value;
     palabras.push(elemento.value);
     elemento.value="";
-    imprimepalabras();
+
+    codeHTML+= "<div>"+valor+"</div>";
+    document.getElementById('palabras').innerHTML=codeHTML;
+    
+
 }
 
 function imprimepalabras(){
     let codeHTML="";
  palabras.forEach((valor, posicion) => {
-     
+     //codeHTML+= ´<div>${posicion} = ${valor}</div>´; 
     codeHTML+= "<div>"+valor+"</div>";
      
  });
